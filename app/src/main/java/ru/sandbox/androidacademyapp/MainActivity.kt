0937270d74ae5b389e9946358cd52533b319ce14
Ansar Kadyrov <j423252@gmail.com>
2020-11-14 00:00:29 +0300

    Add movie details page.

--- app/src/main/java/ru/sandbox/androidacademyapp/MainActivity.kt
+++ app/src/main/java/ru/sandbox/androidacademyapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.sandbox.androidacademyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView: ImageView = findViewById(R.id.avengers_image)
+        imageView.setOnClickListener { movetoMovieDetails() }
+    }
+
+    private fun movetoMovieDetails() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
