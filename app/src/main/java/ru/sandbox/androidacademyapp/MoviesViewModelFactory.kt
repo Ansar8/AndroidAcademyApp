bbcc5c9c788ec738fec5a0cbfdd1b5c396f72312
Ansar Kadyrov <j423252@gmail.com>
2021-01-23 15:56:05 +0300

    Refactor view model factory.

--- app/src/main/java/ru/sandbox/androidacademyapp/MoviesViewModelFactory.kt
+++ app/src/main/java/ru/sandbox/androidacademyapp/MoviesViewModelFactory.kt
@@ -9,8 +9,13 @@ import ru.sandbox.androidacademyapp.repository.MovieRepository
 
 class MoviesViewModelFactory(private val applicationContext: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = when (modelClass) {
-        MoviesViewModel::class.java -> MoviesViewModel(
-            MovieRepository(MoviesApi.create(), MovieDatabase.create(applicationContext).moviesDao))
+        MoviesViewModel::class.java ->
+            MoviesViewModel(
+                MovieRepository(
+                    MoviesApi.create(),
+                    MovieDatabase.create(applicationContext).moviesDao
+                )
+            )
         else -> throw IllegalArgumentException("$modelClass is not registered ViewModel")
     } as T
 }
