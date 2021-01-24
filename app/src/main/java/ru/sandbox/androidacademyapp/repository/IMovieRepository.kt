0a85c93345a183f3ed63ad118497dac80fb30899
Ansar Kadyrov <j423252@gmail.com>
2021-01-24 10:53:31 +0300

    Udpdate movie repository.

--- app/src/main/java/ru/sandbox/androidacademyapp/repository/IMovieRepository.kt
+++ app/src/main/java/ru/sandbox/androidacademyapp/repository/IMovieRepository.kt
@@ -2,11 +2,15 @@ package ru.sandbox.androidacademyapp.repository
 
 import ru.sandbox.androidacademyapp.api.ActorResponse
 import ru.sandbox.androidacademyapp.api.Result
+import ru.sandbox.androidacademyapp.data.db.entites.Actor
 import ru.sandbox.androidacademyapp.data.db.entites.Movie
+import ru.sandbox.androidacademyapp.data.db.entites.relations.MovieWithActors
 
 interface IMovieRepository {
-    suspend fun getPopularMovies(): Result<List<Movie>>
-    suspend fun getSavedPopularMovies(): List<Movie>
-    suspend fun savePopularMovies(movies: List<Movie>)
-    suspend fun getActors(movie_id: Int): List<ActorResponse>
+    suspend fun getMovies(): Result<List<Movie>>
+    suspend fun getMovieWithActors(movieId: Int): Result<MovieWithActors>
+    suspend fun getSavedMovies(): List<Movie>
+    suspend fun getSavedMovieWithActors(movieId: Int): List<MovieWithActors>
+    suspend fun saveMovies(movies: List<Movie>)
+    suspend fun saveMovieWithActors(movieWithActors: MovieWithActors)
 }
--- app/src/main/java/ru/sandbox/androidacademyapp/repository/MovieRepository.kt
+++ app/src/main/java/ru/sandbox/androidacademyapp/repository/MovieRepository.kt
@@ -8,44 +8,79 @@ import ru.sandbox.androidacademyapp.api.ActorResponse
 import ru.sandbox.androidacademyapp.api.MovieResponse
 import ru.sandbox.androidacademyapp.api.Result
 import ru.sandbox.androidacademyapp.data.db.MoviesDao
+import ru.sandbox.androidacademyapp.data.db.entites.Actor
 import ru.sandbox.androidacademyapp.data.db.entites.Movie
+import ru.sandbox.androidacademyapp.data.db.entites.relations.MovieActorCrossRef
+import ru.sandbox.androidacademyapp.data.db.entites.relations.MovieWithActors
+import kotlin.math.roundToInt
 
 class MovieRepository(
     private val moviesApi: MoviesApi,
     private val moviesDao: MoviesDao): IMovieRepository {
 
-    override suspend fun getPopularMovies(): Result<List<Movie>> = withContext(Dispatchers.IO) {
-        try {
-            val response = moviesApi.getMovies()
-            val responseWithDetails = response.movies.map { moviesApi.getMovieDetails(it.id) }
-            val movies = responseWithDetails.map { toEntity(it) }
-            Result.Success(movies)
+    override suspend fun getMovies(): Result<List<Movie>> =
+        withContext(Dispatchers.IO) {
+            try {
+                val response = moviesApi.getMovies()
+                val responseWithDetails = response.movies.map { moviesApi.getMovieDetails(it.id) }
+                val movies = responseWithDetails.map { toMovieEntity(it) }
+                Result.Success(movies)
+            }
+            catch (t: Throwable) {
+                Result.Error("Oops..looks like network failure!")
+            }
         }
-        catch (t: Throwable) {
-            Result.Error("Oops..looks like network failure!")
+
+    override suspend fun getMovieWithActors(movieId: Int): Result<MovieWithActors> =
+        withContext(Dispatchers.IO) {
+            try {
+                val actors = moviesApi.getMovieActors(movieId).actors.map { toActorEntity(it) }
+                val movie = toMovieEntity(moviesApi.getMovieDetails(movieId))
+                val movieWithActors = MovieWithActors(movie, actors)
+
+                Result.Success(movieWithActors)
+            }
+            catch (t: Throwable){
+                Result.Error("Oops..looks like network failure!")
+            }
         }
-    }
 
-    override suspend fun savePopularMovies(movies: List<Movie>){
-        moviesDao.insertMovies(movies)
-    }
-
-    override suspend fun getActors(movie_id: Int): List<ActorResponse> {
-        val response = moviesApi.getMovieActors(movie_id)
-        return response.actors.take(10)
-    }
-
-    override suspend fun getSavedPopularMovies(): List<Movie>{
+    override suspend fun getSavedMovies(): List<Movie>{
         return moviesDao.getPopularMovies()
     }
 
-    private fun toEntity(movie: MovieResponse) = Movie(
+    override suspend fun getSavedMovieWithActors(movieId: Int): List<MovieWithActors> {
+        return moviesDao.getMovieWithActors(movieId)
+    }
+
+    override suspend fun saveMovies(movies: List<Movie>){
+        moviesDao.insertMovies(movies)
+    }
+
+    override suspend fun saveMovieWithActors(movieWithActors: MovieWithActors) {
+        val movie = movieWithActors.movie
+        val actors = movieWithActors.actors
+
+        moviesDao.insertActors(actors)
+        actors.forEach { actor ->
+            val crossRef = MovieActorCrossRef(movie.id, actor.id)
+            moviesDao.insertMovieActorCrossRef(crossRef)
+        }
+    }
+
+    private fun toActorEntity(actor: ActorResponse) = Actor(
+        id = actor.id,
+        name = actor.name,
+        pictureUrl = BuildConfig.IMAGES_BASE_URL + BuildConfig.PROFILE_SIZE + actor.picture
+    )
+
+    private fun toMovieEntity(movie: MovieResponse) = Movie(
         id = movie.id,
         title = movie.title,
         overview = movie.overview,
         posterUrl = BuildConfig.IMAGES_BASE_URL + BuildConfig.POSTER_SIZE + movie.poster,
         backdropUrl = BuildConfig.IMAGES_BASE_URL + BuildConfig.BACKDROP_SIZE + movie.backdrop,
-        ratings = movie.ratings,
+        ratings = movie.ratings.div(10).times(5).roundToInt(),
         reviews = movie.reviews,
         minAge = if (movie.adult) 16 else 13,
         runtime = movie.runtime,
