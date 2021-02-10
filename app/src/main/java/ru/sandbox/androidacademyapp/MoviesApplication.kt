e8a7bad80fbc591c8aaec72e538fd79eb03d419a
Ansar Kadyrov <j423252@gmail.com>
2021-02-10 20:07:06 +0300

    Refactor the code.

--- app/src/main/java/ru/sandbox/androidacademyapp/MoviesApplication.kt
+++ app/src/main/java/ru/sandbox/androidacademyapp/MoviesApplication.kt
@@ -15,8 +15,13 @@ class MoviesApplication: Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
 
-        // Use a one time work request to check that WM works as expected
-        // instead of managing cancellation of periodic request after app is tested.
+        //setOneTimeCacheUpdate()
+    }
+
+    fun setOneTimeCacheUpdate(){
+        //Use a one time work request to check that WM works as expected
+        //instead of managing cancellation of periodic request after app is tested.
+
         val simpleRequest = OneTimeWorkRequest.Builder(UpdateMoviesWorker::class.java)
                 .setInitialDelay(5, TimeUnit.MINUTES)
                 .setConstraints( Constraints.Builder()
@@ -25,36 +30,39 @@ class MoviesApplication: Application(), Configuration.Provider {
                     .build()
                 ).build()
 
-        // Track the live data of a work request state
+        //Track the live data of a work request state
+
         WorkManager.getInstance(this)
             .getWorkInfoByIdLiveData(simpleRequest.id)
             .observeForever { workInfo ->
                 Log.d("-> state", workInfo.state.toString())
             }
 
-        // Schedule a one time work request
+        //Schedule a one time work request
         WorkManager.getInstance(this).enqueue(simpleRequest)
+    }
 
-        // An example how to enqueue unique periodic work
+    fun setPeriodicCacheUpdate(){
+         //An example how to enqueue unique periodic work
 
-        // val updateMoviesWorkRequest = PeriodicWorkRequest
-        //     .Builder(UpdateMoviesWorker::class.java, 8, TimeUnit.HOURS)
-        //     .setConstraints( Constraints.Builder()
-        //         .setRequiresCharging(true)
-        //         .setRequiredNetworkType(NetworkType.UNMETERED)
-        //         .build()
-        //     )
-        //     .build()
+         val updateMoviesWorkRequest = PeriodicWorkRequest
+             .Builder(UpdateMoviesWorker::class.java, 8, TimeUnit.HOURS)
+             .setConstraints( Constraints.Builder()
+                 .setRequiresCharging(true)
+                 .setRequiredNetworkType(NetworkType.UNMETERED)
+                 .build()
+             )
+             .build()
 
 
-        // WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-        //     "updateMoviesWork",
-        //     ExistingPeriodicWorkPolicy.KEEP,
-        //     updateMoviesWorkRequest
-        // )
+         WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+             "updateMoviesWork",
+             ExistingPeriodicWorkPolicy.KEEP,
+             updateMoviesWorkRequest
+         )
 
-        // An example how to cancel a unique periodic work by it's name
-        // WorkManager.getInstance(this).cancelUniqueWork("updateMoviesWork")
+         //An example how to cancel a unique periodic work by it's name
+         WorkManager.getInstance(this).cancelUniqueWork("updateMoviesWork")
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
